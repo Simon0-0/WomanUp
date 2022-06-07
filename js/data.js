205e1a3096404be2e9dc8191e12cff397f8d2cc4
Simon0-0 <97345020+Simon0-0@users.noreply.github.com>
2022-06-07 20:46:51 +0200

    burger zaciatok plus podcast

---
+++ js/data.js
@@ -0,0 +1,59 @@
+const episodes = [
+    {
+      0: {
+        date: "15. 5. 2022",
+        minute: "56:13",
+        picture: ""
+      },
+      1: {
+        date: "26. 1. 2022",
+        minute: "40:19",
+        picture: ""
+      },
+      2: {
+        date: "12. 1. 2022",
+        minute: "43:17",
+        picture: ""
+      },
+      3: {
+        date: "30. 12. 2021",
+        minute: "55:33",
+        picture: ""
+      },
+      4: {
+        date: "17. 12. 2021",
+        minute: "1:04:47",
+        picture: ""
+      },
+      5: {
+          date: "1. 12. 2021",
+      minute: "41:04",
+      picture: ""
+      },
+      6: {
+        date: "17. 11. 2021",
+        minute: "47:56",
+        picture: ""
+      },
+      7: {
+        date: "28. 10. 2021",
+        minute: "37:28",
+        picture: ""
+      },
+      8: {
+        date: "13. 10. 2021",
+        minute: "46:27",
+        picture: ""
+      },
+      9: {
+        date: "28. 9. 2021",
+        minute: "57:34",
+        picture: ""
+      },
+      10: {
+        date: "15. 9. 2021",
+        minute: "43:36",
+        picture: ""
+      }
+    }
+];
