5504f4a2e4c4e98d84023377436fcedba8bf0c90
Simon0-0 <97345020+Simon0-0@users.noreply.github.com>
2022-06-14 15:32:04 +0200

    can I die now?

--- js/style.js
+++ js/style.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
 
     function mainVid(id) {
         $("#video").html(`
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}"
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/${id}"
                 title="YouTube video player" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen></iframe>
